0946a9f53ba725f592eb06ab8161fc975087f008
Suo Qin <kpsesuo@gmail.com>
2017-08-09 23:57:03 +1000

    mutate state

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,25 @@
 <template>
   <div>
     {{counter}}
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
   </div>
 </template>
 
 <script>
-import {mapState} from 'vuex'
+import {mapState, mapMutations} from 'vuex'
 
 export default {
   computed: {
     ...mapState({
       counter: state => state.counter
     })
+  },
+  methods: {
+    ...mapMutations([
+      'increment',
+      'decrement'
+    ])
   }
 }
 </script>
--- store/index.js
+++ store/index.js
@@ -1,3 +1,12 @@
-export const state = function ()  {
+export const state = function () {
   return {counter: 2}
+}
+
+export const mutations = {
+  increment (state) {
+    state.counter++
+  },
+  decrement (state) {
+    state.counter--
+  }
 }
